18174b3fa53f445a474a9ae374b8ee641fce94fb
QiYao <oqiyao@gmail.com>
2019-01-17 01:01:34 +0800

    结构调整

--- pdb.py
+++ pdb.py
@@ -3,35 +3,9 @@ import os
 from sklearn.externals import joblib
 import matplotlib.pyplot as plt
 from scipy.spatial import procrustes
+from face_dataset import utils
 np.random.seed(0)
 os.system('mkdir cache')
-
-
-def read_bbox(path):
-    """
-    从文件读取bounding box 坐标
-    :param path: rect文件路径
-    :return: [min_x, min_y, max_x, max_y]
-    """
-    with open(path) as f:
-        l = f.readline().strip('\n').split(' ')
-    return [int(ll) for ll in l]
-
-
-def read_landmarks(path):
-    """
-    从文件读取landmark坐标
-    :param path: txt文件路径
-    :return: 形状为(106, 2)的numpy.ndarray
-    """
-    landmarks = []
-    with open(path) as f:
-        n = int(f.readline())
-        for i in range(n):
-            pt = f.readline().strip('\n').split(' ')
-            pt = [int(float(p)) for p in pt]
-            landmarks.append(pt)
-    return np.array(landmarks, dtype=np.float32)
 
 
 def procrustes(x, y):
@@ -84,14 +58,9 @@ def main():
                 split[id] = 'valid'
             landmark_path = os.path.join(lamdmark_dir, filename + '.txt')
             bbox_path = os.path.join(bbox_dir, filename + '.rect')
-            bbox = read_bbox(bbox_path)
-            minx, miny, maxx, maxy = bbox
-            w = float(maxx - minx)
-            h = float(maxy - miny)
-            landmarks = read_landmarks(landmark_path)
-            landmarks -= [minx, miny]
-            landmarks[:, 0] = landmarks[:, 0] / w
-            landmarks[:, 1] = landmarks[:, 1] / h
+            bbox = utils.read_bbox(bbox_path)
+            landmarks = utils.read_landmarks(landmark_path)
+            landmarks = utils.norm_landmarks(landmarks, bbox)
             norm_landmarks.append(landmarks)
             bboxes.append(bbox)
         norm_landmarks = np.stack(norm_landmarks, axis=0)
