28ef0d5d6f44b806a932f113ad459ecc5846cb11
Zhang Minjian <1519739319@qq.com>
2019-01-16 15:56:30 +0800

    NME by zhangminjian

--- utils.py
+++ utils.py
@@ -1,19 +1,28 @@
 import numpy as np
+M = 4
+N = 106
+G = np.random.rand(M, N, 2)  # ground truth (三维数组,M代表图的数量,N代表关键点数量)
+P = np.random.rand(M, N, 2)  # 预测值
+
+GMax = np.max(G, axis=1)
+GMin = np.min(G, axis=1)
+WH = GMax - GMin
+W = WH[:, 0]
+H = WH[:, 1]
+S = np.sqrt(W*H)   # 所有图中ground truth 的面积构成的数组
 
 
-src = np.random.rand(3, 68, 2)
-dst = np.random.rand(3, 68, 2)
+A = G - P
+B = A**2
+C = B.sum(axis=2)
+D = np.sqrt(C)
+D1 = D.sum(axis=1)
+D2 = D1 / S
+D3 = D2 / N    # N表示点的数目,D3是一个数组,其中的元素是每个图的NME
+
+E = np.sum(D3)
+F = E / M      # M表示图的数目,F是整个图集的NME
 
 
-def calc_nme(src, dst):
-
-    bbox = np.max(dst, axis=-2) - np.min(dst, axis=-2)
-    euler = np.sqrt(np.sum((src - dst)**2, axis=-1))
-    w = 1 / np.sqrt(bbox[:, 0] * bbox[:, 1])
-    nme = w * np.mean(euler, axis=-1)
-    return nme
-
-def calc_auc():
-    pass
-
-calc_nme(src, dst)
+print(D3)
+print(F)
