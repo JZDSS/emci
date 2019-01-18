a9cf34bc1bb35c66e599b2ed1cb31e849d7d3c85
LiBoqi <liboqi1996@126.com>
2019-01-18 11:11:56 +0800

    'usenewnmeforevalandnewsaverload'

--- eval.py
+++ eval.py
@@ -1,27 +1,27 @@
 from data.face_dataset import FaceDataset
 import torch
-import torchvision.models as models
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from layers.module.wing_loss import WingLoss
-import utils
+from models.saver import Saver
+from models.resnet50 import ResNet50
+
 import numpy as np
-net =models.resnet18(num_classes=212,pretrained=False)
+from utils.metrics import Metrics
+net = ResNet50()
 sig = nn.Sigmoid()
 
-PATH = './modelsave/90'
+#PATH = './ckpt'
 a = FaceDataset("E:/correctdata", "E:/correctdata/valid")
 batch_iterator = iter(DataLoader(a, batch_size=4, shuffle=True, num_workers=0))
-
-
-checkpoint =torch.load(PATH)
-model = net
-model.load_state_dict(checkpoint['net'])#optimizer.load_state_dict(checkpoint['optimizer'])
-model.eval()
+#Saver.dir=PATH
+saver = Saver('ckpt', 'model', 10)
+saver.load_last_ckpt(net)
+net.eval()
 batch_size = 4
 epoch_size = len(a) // batch_size
-Z=[]
-Q=[]
+
+metrics = Metrics().add_nme(0.99).add_auc()
 for iteration in range(11):
     if iteration % epoch_size == 0:
         batch_iterator = iter(DataLoader(a, batch_size,
@@ -31,19 +31,13 @@ for iteration in range(11):
     with torch.no_grad():
         out = net.forward(images)
     out = sig(out)
-    #print(utils.nme_calc(landmarks,out))
-
     criterion = WingLoss(10, 0.5)
     loss = criterion(out, landmarks)
-    out = out.numpy()
-    landmarks = landmarks.numpy()
-    Z.append(out)
-    Q.append(landmarks)
+    pr = out.cpu().data.numpy()
+    gt = landmarks.cpu().data.numpy()
 
     if iteration % 10 == 0:
         #print(loss.item())
-        X=np.stack(Z,axis=0)
-        X = out.reshape(-1, 106, 2)
-        Y = landmarks.reshape(-1, 106, 2)
-        print(utils.auc_calc(utils.nme_calc(Y, X)))
+        metrics.nme.update(np.reshape(gt, (-1, 106, 2)), np.reshape(pr, (-1, 106, 2)))
+
 
