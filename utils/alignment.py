016b82ea23766d5d318a32b252681e12a46727d1
orion-github <3064817282@qq.com>
2019-01-19 16:22:54 +0800

    修改

--- utils/alignment.py
+++ utils/alignment.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.externals import joblib
 import pdb
 import cv2
-import data.face_dataset as fd
+import data.read_dataset as fd
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 import time
@@ -12,44 +12,72 @@ from PIL import Image
 
 mean_landmarks = joblib.load('../cache/mean_landmarks.pkl')
 
+def inv_norm_landmark(landmark, bbox):
+    """
+    根据bounding box信息对landmarks坐标进行规范化。
+    :param landmarks: 形如(N, 2)的矩阵，其中N为landmark个数
+    :param bbox: [minx, miny, maxx, maxy]
+    :return: 规范化landmarks，形状同landmarks输入。
+    """
+    minx, miny, maxx, maxy = bbox
+    w = float(maxx - minx)
+    h = float(maxy - miny)
+    landmark[:, 0] = landmark[:, 0] * w
+    landmark[:, 1] = landmark[:, 1] * h
+    landmark += [minx, miny]
+    return landmark
+
 def align(images, landmarks, bboxs):
     ones = np.ones(106)
     batch_size = int(images.shape[0])
+
+    bboxs_origin = bboxs.data.numpy()
+
     for i in range(batch_size):
         image = images[i].data.numpy()
-        image = np.transpose(image, (1, 2, 0)).astype(np.uint8).copy()
-        plt.imshow(image)
-        plt.show()
 
         landmark = landmarks[i].data.numpy()
         landmark = landmark.reshape((106, 2), order='C')
         #landmark = np.reshape(landmark, (106, 2))
+        bbox = bboxs_origin[i].astype(np.int)
+        landmark = inv_norm_landmark(landmark, np.reshape(bbox, (-1))).astype(np.int)
+        image = np.transpose(image, (1, 2, 0)).astype(np.uint8).copy()
 
-        bbox = bboxs[2*i : 2*i+2]#(1, 4)
-        bbox1 = bbox[0].data.numpy()
-        bbox2 = bbox[1].data.numpy()
-        bbox = np.r_[bbox1, bbox2]
-        bbox = bbox.reshape((2, 2))
-        bbox = np.c_[bbox, [1, 1]]
-
-        x = np.c_[landmark, ones]
-        T = pdb.procrustes(x, mean_landmarks)
-        cols, rows, ch = image.shape
-        image = cv2.warpAffine(image, np.transpose(T), (cols, rows))
+        plt.figure()
         plt.imshow(image)
+        plt.scatter(landmark[:, 0], landmark[:, 1])
+        plt.plot(bbox[:, 0], bbox[:, 1])
         plt.show()
 
-        bbox = bbox@T
 
-        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
-        images[i] = torch.from_numpy(image)
-        time.sleep(3)
+        x = np.c_[landmark, ones]
+        T = pdb.procrustes(x, mean_landmarks*128)
+        landmark = x @ T
+
+        bbox = bbox.reshape((2, 2))
+        bbox = np.c_[bbox, [1, 1]]
+        bbox = bbox @ T
+
+        cols, rows, ch = image.shape
+        image = cv2.warpAffine(image, np.transpose(T), (cols, rows))
+
+
+        plt.figure()
+        plt.imshow(image)
+        # plt.show()
+        plt.scatter(landmark[:, 0], landmark[:, 1])
+        plt.plot(bbox[:, 0], bbox[:, 1])
+
+        plt.xlim(0, 128)
+        plt.ylim(128, 0)
+        plt.show()
+
 
     return images
 
 if __name__ == "__main__":
-    a = fd.FaceDataset("/data/icme", "/data/icme/train")
-    b = iter(DataLoader(a, batch_size=2, shuffle=True, num_workers=0))
+    a = fd.FaceDataset("../data/icme", "../data/icme/train")
+    b = iter(DataLoader(a, batch_size=100, shuffle=True, num_workers=0))
     while True:
         images, landmarks, bboxs = next(b)
         images = align(images, landmarks, bboxs)
