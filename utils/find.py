3cf23b6d4af2d5908e313843d867601761a49a72
orion-github <3064817282@qq.com>
2019-01-20 10:46:45 +0800

    修改

--- utils/find.py
+++ utils/find.py
@@ -15,26 +15,25 @@ import matplotlib.pyplot as plt
 batch_size = 4
 metrics = Metrics().add_nme(0.9).add_auc(decay=0.9).add_loss(decay=0.9)
 
-
 if __name__ == "__main__":
     current = None
     net = ResNet18().cuda()
     net.load_state_dict(torch.load('./ckpt/model-9200.pth'))
     net.eval()
-    NME = np.empty(11)
-    AUC = np.empty(11)
+    NME = np.zeros(11)
+    AUC = np.zeros(11)
     for pose in range(11):
         a = PoseDataset("/home/orion/Desktop/ecmi/emci/data/icme", "/home/orion/Desktop/ecmi/emci/data/icme/valid",
                         phase='eval', pose=pose)
-        batch_iterator = iter(DataLoader(a, batch_size=4, shuffle=True, num_workers=4))
+        index = int(a.get_index())
+        batch_iterator = iter(DataLoader(a, batch_size=1, shuffle=False, num_workers=0))
         iterations = math.ceil(len(a) / batch_size)
         images = None
         landmarks = None
         gt = None
         pr = None
-        interation = 0
-        nmes = np.empty(iterations)
-        aucs = np.empty(iterations)
+        pr_list = []
+        gt_list = []
         while True:
             try:
                 images, landmarks = next(batch_iterator)
@@ -45,28 +44,30 @@ if __name__ == "__main__":
                 #loss = criterion(out, landmarks)
                 pr = out.cpu().data.numpy()
                 gt = landmarks.cpu().data.numpy()
-                nme = metrics.nme.update(np.reshape(gt, (-1, 106, 2)), np.reshape(pr, (-1, 106, 2)))
-                auc = metrics.auc.update(nme)
-                nmes[interation] = np.mean(nme)
-                aucs[interation] = auc
-                #print('nme', nme, 'auc', auc, nmes[interation], aucs[interation])
-                #metrics.loss.update(loss.item())
-                metrics.clear()
-                interation += 1
+                pr_list.append(pr)
+                gt_list.append(gt)
             except StopIteration:
                 break
-        print('pose:', pose, 'len:', len(a))
-        NME[pose] = np.mean(nmes)
-        AUC[pose] = np.mean(aucs)
+        gt_array = np.reshape(np.array(gt_list), (-1, 106, 2))
+        pr_array = np.reshape(np.array(pr_list), (-1, 106, 2))
+        nme = metrics.nme.update(gt_array, pr_array)
+        auc = metrics.auc.update(nme)
+        NME[index - 1] = np.mean(nme)
+        AUC[index - 1] = auc
+        metrics.clear()
+        print('pose:', pose + 1, 'len:', len(a), 'index:', index)
+        # print('nme', nme, 'auc', auc, nmes[interation], aucs[interation])
+        # metrics.loss.update(loss.item())
+
     x = np.linspace(1, 11, 11)
     plt.plot(x, NME, )
     plt.title('NME')
-    plt.xlabel = 'pose'
-    plt.ylabel = 'NME'
+    plt.xlim((0, 12))
+    plt.grid()
     plt.show()
 
     plt.plot(x, AUC, )
     plt.title('AUC')
-    plt.xlabel = 'pose'
-    plt.ylabel = 'AUC'
+    plt.xlim((0, 12))
+    plt.grid()
     plt.show()
