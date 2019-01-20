bdcf55cebab83ff22894a7a9f3b964678b44c61a
QiYao <oqiyao@gmail.com>
2019-01-20 20:42:35 +0800

    fix bug

--- utils/mining_landmark.py
+++ utils/mining_landmark.py
@@ -16,12 +16,12 @@ metrics = Metrics().add_nme(0.9).add_auc(decay=0.9).add_loss(decay=0.9)
 
 if __name__ == "__main__":
     net = ResNet18().cuda()
-    net.load_state_dict(torch.load('./ckpt/model-9200.pth'))
+    net.load_state_dict(torch.load('../backup/model-9200.pth'))
     net.eval()
     PR_list = []
     GT_list = []
     for pose in range(11):
-        a = PoseDataset("/home/orion/Desktop/ecmi/emci/data/icme", "/home/orion/Desktop/ecmi/emci/data/icme/valid",
+        a = PoseDataset("/data/icme", "/data/icme/valid",
                         phase='eval', pose=pose)
         index = int(a.get_index())
         batch_iterator = iter(DataLoader(a, batch_size=1, shuffle=False, num_workers=0))
@@ -45,47 +45,86 @@ if __name__ == "__main__":
                 break
         print('pose:', pose + 1, 'len:', len(a), 'index:', index)
 
-    M =len(GT_list)
-    G = []
-    P = []
-    for i in range(M):
-        G.append(np.reshape(GT_list[i], (106, 2), order='F'))
-        P.append(np.reshape(PR_list[i], (106, 2), order='F'))
-    GT_array = np.array(G)
-    PR_array = np.array(P)
+    def calc_nme(G, P):
+        GMax = np.max(G, axis=1)
+        GMin = np.min(G, axis=1)
+        WH = GMax - GMin
+        W = WH[:, 0]
+        H = WH[:, 1]
+        S = np.sqrt(W * H)  # 所有图中ground truth 的面积构成的数组
 
-    N = GT_array.shape[1]#关键点数
+        A = G - P
+        B = A ** 2
+        C = B.sum(axis=2)
+        D = np.sqrt(C)
+        D /= S
+        return D
 
-    NME = np.zeros(N)
-    AUC = np.zeros(N)
-    for n in range(N):
-        GT_list = []
-        PR_list = []
-        for m in range(M):
-            GT_list.append(GT_array[m, n, :])
-            PR_list.append(PR_array[m, n, :])
-        GT = np.array(GT_list)
-        PR = np.array(PR_list)
-        GT = np.reshape(GT, (1, -1, 2))
-        PR = np.reshape(PR, (1, -1, 2))
-        nmes = metrics.nme.update(GT, PR)
-        auc = metrics.auc.update(nmes)
-        nme = np.mean(nmes)
-        NME[n] = nme
-        AUC[n] = auc
-        metrics.clear()
-
-    print("NME:", NME)
-    print("AUC:", AUC)
+    nmes = []
+    # for gt, pr in zip(GT_list, PR_list):
+    for i in range(len(GT_list)):
+        gt = GT_list[i]
+        pr = PR_list[i]
+        nme = calc_nme(np.reshape(gt, (1, 106, 2)), np.reshape(pr, (1, 106, 2)))
+        nmes.append(nme)
+    nmes = np.concatenate(nmes, axis=0)
+    nmes_ = np.mean(nmes, axis=0)
+    plt.subplot(1, 2, 1)
     x = np.linspace(1, 106, 106)
-    plt.plot(x, NME, )
+    plt.plot(x, nmes_)
     plt.title('NME')
-    plt.xlim((0, 107))
-    plt.grid()
-    plt.show()
-
-    plt.plot(x, AUC, )
+    aucs = []
+    for i in range(106):
+        nme = nmes[:, i]
+        aucs.append(metrics.auc.update(nme))
+        metrics.clear()
+    plt.subplot(1,2,2)
+    plt.plot(x, aucs)
     plt.title('AUC')
-    plt.xlim((0, 107))
-    plt.grid()
     plt.show()
+    a = 1
+
+    # M =len(GT_list)
+    # G = []
+    # P = []
+    # for i in range(M):
+    #     G.append(np.reshape(GT_list[i], (106, 2), order='F'))
+    #     P.append(np.reshape(PR_list[i], (106, 2), order='F'))
+    # GT_array = np.array(G)
+    # PR_array = np.array(P)
+    #
+    # N = GT_array.shape[1]#关键点数
+    #
+    # NME = np.zeros(N)
+    # AUC = np.zeros(N)
+    # for n in range(N):
+    #     GT_list = []
+    #     PR_list = []
+    #     for m in range(M):
+    #         GT_list.append(GT_array[m, n, :])
+    #         PR_list.append(PR_array[m, n, :])
+    #     GT = np.array(GT_list)
+    #     PR = np.array(PR_list)
+    #     GT = np.reshape(GT, (1, -1, 2))
+    #     PR = np.reshape(PR, (1, -1, 2))
+    #     nmes = metrics.nme.update(GT, PR)
+    #     auc = metrics.auc.update(nmes)
+    #     nme = np.mean(nmes)
+    #     NME[n] = nme
+    #     AUC[n] = auc
+    #     metrics.clear()
+    #
+    # print("NME:", NME)
+    # print("AUC:", AUC)
+    # x = np.linspace(1, 106, 106)
+    # plt.plot(x, NME, )
+    # plt.title('NME')
+    # plt.xlim((0, 107))
+    # plt.grid()
+    # plt.show()
+    #
+    # plt.plot(x, AUC, )
+    # plt.title('AUC')
+    # plt.xlim((0, 107))
+    # plt.grid()
+    # plt.show()
