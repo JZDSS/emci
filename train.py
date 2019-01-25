866e5f6af23ee97d6d2883f22e31d9b431b720c6
QiYao <oqiyao@gmail.com>
2019-01-25 22:07:51 +0800

    train and eval script using new dataset

--- train.py
+++ train.py
@@ -8,26 +8,28 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 
 from data.utils import draw_landmarks
-from data.face_dataset import FaceDataset
-from data.pose_dataset import PoseDataset
+from data.bbox_dataset import BBoxDataset
+from data.align_dataset import AlignDataset
 from layers.module.wing_loss import WingLoss
 from layers.module.gyro_loss import GyroLoss
 
 from models.saver import Saver
 from models.resnet50 import ResNet50
 from models.resnet18 import ResNet18
+from models.dense201 import Dense201
 from utils.metrics import Metrics
+from utils.alignment import Align
 
 
 parser = argparse.ArgumentParser(
     description='Landmark Detection Training')
 
-parser.add_argument('-l', '--lr', default=1e-5, type=float)
+parser.add_argument('-l', '--lr', default=5e-6, type=float)
 parser.add_argument('-b', '--batch_size', default=16, type=int)
 parser.add_argument('-c', '--cuda', default=True, type=bool)
 parser.add_argument('-n', '--n_gpu', default=1, type=int)
-parser.add_argument('-s', '--step', default=120000, type=int)
-parser.add_argument('-g', '--gamma', default=0.1, type=float)
+parser.add_argument('-s', '--step', default=60000, type=int)
+parser.add_argument('-g', '--gamma', default=0.2, type=float)
 parser.add_argument('-w', '--weight_decay', default=5e-4, type=float)
 
 args = parser.parse_args()
@@ -49,14 +51,22 @@ def adjust_learning_rate(optimizer, step, gamma, epoch, iteration, epoch_size):
 if __name__ == '__main__':
     metrics = Metrics().add_nme(0.5).add_auc(decay=0.5).add_loss(decay=0.5)
 
-    writer = SummaryWriter('logs/wing_loss_res18/train')
-    net = ResNet18().cuda()
-    a = PoseDataset("/home/zhzhong/Desktop/correctdata", "/home/zhzhong/Desktop/correctdata/train", pose=1)
+    writer = SummaryWriter('logs/densealignbydense/train')
+    net = Dense201().cuda()
+    # a = BBoxDataset('/data/icme/data/picture',
+    #                 '/data/icme/data/landmark',
+    #                 '/data/icme/bbox',
+    #                 '/data/icme/train')
+    a = AlignDataset('/data/icme/data/picture',
+                 '/data/icme/data/landmark',
+                 '/data/icme/data/landmark',
+                 '/data/icme/valid',
+                 Align('../cache/mean_landmarks.pkl', (224, 224), (0.15, 0.05)))
     batch_iterator = iter(DataLoader(a, batch_size=args.batch_size, shuffle=True, num_workers=4))
 
     criterion = WingLoss(10, 2)
     optimizer = optim.Adam(net.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    saver = Saver('ckpt', 'model', 10)
+    saver = Saver('../ckpt', 'model', 10)
     last = saver.last_ckpt()
     start_iter = 0 if last is None else int(last.split('.')[0].split('-')[-1])
     if start_iter > 0:
@@ -102,7 +112,7 @@ if __name__ == '__main__':
             writer.add_scalar("watch/learning_rate", lr, iteration)
 
             writer.add_image("result", image, iteration)
-            writer.add_histogram("prediction", out.cpu().data.numpy(), iteration)
+            writer.add_histogram("predictionx", out.cpu().data.numpy()[:, 0:212:2], iteration)
             state = net.state_dict()
             saver.save(state, iteration)
 
