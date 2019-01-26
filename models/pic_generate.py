6de26cad719978c867e76997969fe138996fbd78
zhzhong11 <zhongzihan_thu@163.com>
2019-01-27 00:20:35 +0800

    debug

--- models/pic_generate.py
+++ models/pic_generate.py
@@ -4,25 +4,28 @@ from data import utils
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = os.listdir("/home/zhang/PWA-AL/1/")
-base_dir = "/home/zhang/PWA-AL/1/"
-new_dir = "/home/zhang/PWA-AL/2/"
+filename = os.listdir("/home/zhzhong/Desktop/correctdata/train/1/")
+img_dir = '/home/zhzhong/Desktop/correctdata/train/1/'
+base_dir = "/home/zhzhong/Desktop/correctdata/bbox/"
+new_dir = "/home/zhzhong/Desktop/newdata/1/"
 bbox1 = np.array([0, 0, 128, 128])
 bbox2 = np.array([0, 0, 128, 128])
-dst = utils.read_landmarks('/home/zhang/aligned/landmark/AFW_261068_1_1.jpg.txt')
+dst = utils.read_mat('/home/zhzhong/Desktop/correctdata/data/landmark/AFW_5452623_1_5.jpg.txt')
 dst = utils.norm_landmarks(dst, bbox2)
-filenameforlmk = os.listdir("/home/zhang/PWA-AL/1lmk/")
-base_diroflmk = "/home/zhang/PWA-AL/1lmk/"
+filenameforlmk = os.listdir("/home/zhzhong/Desktop/correctdata/data/landmark/")
+base_diroflmk = "/home/zhzhong/Desktop/correctdata/data/landmark/"
 for img in filename:
-    image = plt.imread(base_dir + img)
-    image = image[bbox1[1]: bbox1[3], bbox1[0]: bbox1[2]]
+    image = plt.imread(img_dir + img)
+    # plt.imshow(image)
+    # plt.show()
+    box_dir = "/home/zhzhong/Desktop/correctdata/bbox/" + img + '.rect'
+    box = utils.read_bbox(box_dir)
+    image = image[box[1]: box[3], box[0]: box[2]]
     for lmk in filenameforlmk:
-
-        if img == lmk.strip('.txt'):
-            src = utils.read_landmarks(base_diroflmk + lmk)
-            src = utils.norm_landmarks(src, bbox1)
-            image_pwa = pwa.pwa(image, src, dst, (128, 128))
-            #image_pwa.save(new_dir + img)
-            plt.imshow(image_pwa)
-            plt.show()
+        src = utils.read_mat(base_diroflmk + lmk)
+        src = utils.norm_landmarks(src, bbox1)
+        image_pwa,_ = pwa.pwa(image, src, dst, (128, 128))
+        #image_pwa.save(new_dir + img)
+        plt.imshow(image_pwa)
+        plt.show()
 
