c14ec0475ff65a947473ff7ba2046a42d74a6a3e
shw2018 <hwsun@std.uestc.edu.cn>
2019-01-27 23:04:37 +0800

    modify

--- shw/70to36_Ldmk_Mlp.py
+++ shw/70to36_Ldmk_Mlp.py
@@ -8,16 +8,15 @@ from shw.LdmkDataset import LdmkDataset
 import os
 from wing_loss import WingLoss
 
-
 class MLP(nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
 
         # 前71个landmark点，网络要重写
-        self.fc1 = nn.Linear(152, 600)
+        self.fc1 = nn.Linear(132, 600)
         self.fc2 = nn.Linear(600, 300)
         self.fc3 = nn.Linear(300, 150)
-        self.fc4 = nn.Linear(150, 60)
+        self.fc4 = nn.Linear(150, 80)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -26,19 +25,16 @@ class MLP(nn.Module):
         x = self.fc4(x)
         return x
 
-
 mlp = MLP().double()
-
 a = LdmkDataset(os.path.join("icme","data","landmark"))
-batch_loader = DataLoader(a, batch_size=45, shuffle=True, num_workers=0)
+batch_loader = DataLoader(a, batch_size=40, shuffle=True, num_workers=0)
 # batch_iterator = iter(DataLoader(a, batch_size=4, shuffle=True, num_workers=0))
 # criterion = nn.L1Loss()
 # criterion = nn.MSELoss()
-
-criterion = WingLoss(6, 2)
+criterion = WingLoss(5, 2)
 optimizer = opt.Adam(mlp.parameters(), lr=2e-3, weight_decay=5e-4)
 
-for epoch in range(1000):    # loop over the dataset multiple times
+for epoch in range(500):    # loop over the dataset multiple times
 
     running_loss = 0.0
     iteration = 0
