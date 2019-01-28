3ef5581ca19f4baa9f15f51f8aa2153f9bc68dfd
myKevska <myl8562@.163.com>
2019-01-28 19:28:05 +0800

    debug

--- 70to36.py
+++ 70to36.py
@@ -1,4 +1,3 @@
-
 # _*_ coding:utf-8 _*_
 
 from data.face_dataset import FaceDataset
@@ -15,16 +14,14 @@ import numpy as np
 import os
 from layers.module.wing_loss import WingLoss
 
-import torch
-
 class MLP(nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
         # 前70个landmark点，网络要重写
-        self.fc1 = nn.Linear(140, 500)
-        self.fc2 = nn.Linear(500, 225)
-        self.fc3 = nn.Linear(225, 125)
-        self.fc4 = nn.Linear(125, 72)
+        self.fc1 = nn.Linear(114, 800)
+        self.fc2 = nn.Linear(800, 400)
+        self.fc3 = nn.Linear(400, 200)
+        self.fc4 = nn.Linear(200, 98)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -35,28 +32,18 @@ class MLP(nn.Module):
 
 
 mlp = MLP().double()
-
-# a = LBDataset("/home/zhzhong/Desktop/correctdata", "/home/zhzhong/Desktop/correctdata/train")
-
 a = LdmkDataset(os.path.join("/home/orion/correctdata/data", "landmark"))
-
 batch_loader = DataLoader(a, batch_size=4, shuffle=True, num_workers=0)
-# batch_iterator = iter(DataLoader(a, batch_size=4, shuffle=True, num_workers=0))
 # criterion = nn.L1Loss()
 # criterion = nn.MSELoss()
-criterion = WingLoss(10, 2)
+criterion = WingLoss(5, 2)
 optimizer = opt.Adam(mlp.parameters(), lr=2e-3, weight_decay=5e-4)
 
-for epoch in range(10):
+for epoch in range(500):   # loop over the dataset multiple times
     running_loss = 0.0
-# for epoch in range(5000):  # loop over the dataset multiple times
-
     iteration = 0
     for inputs, labels in iter(batch_loader):
-
         # print(inputs, labels)
-        # inputs, labels = next(batch_iterator)
-        # landmarks = landmarks  # .cuda
         out = mlp(inputs).double()
         # print(out.size(), labels.size())
 
@@ -65,6 +52,7 @@ for epoch in range(10):
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
+
         if iteration % 100 == 99:
             print('\r%5d loss: %.3f' %
                   (iteration + 1, running_loss / 100), end="\n")
--- ReadLdmk.py
+++ ReadLdmk.py
@@ -1,4 +1,4 @@
-# _*_ coding:utf-8 _*_
+## _*_ coding:utf-8 _*_
 
 import os
 import cv2
@@ -22,8 +22,15 @@ class LdmkDataset(Dataset):
         landmark = self.read_landmark(self.landmarks[i])
         landmark = landmark.astype(np.float)
         # print(landmark[105])
-        x = (landmark[0:71]).flatten()
-        y = (landmark[71:106]).flatten()
+
+        x = (np.vstack((landmark[0:33],landmark[33],landmark[37:39],landmark[42],landmark[46],landmark[50:58],landmark[63:67],
+                        landmark[70],landmark[75],landmark[79],landmark[84],landmark[87],landmark[90],landmark[93]))).flatten()
+        y = (np.vstack((landmark[34:37],landmark[39:42],landmark[43:46],landmark[47:50],landmark[58:63],landmark[67:70],landmark[71:75],
+                        landmark[76:79],landmark[80:84],landmark[85:87],landmark[88:90],landmark[91:93],landmark[94:106]))).flatten()
+
+        # x = (landmark[0:71]).flatten()
+        # y = (landmark[71:106]).flatten()
+
         # print(x.shape, y.shape)
         return torch.from_numpy(x).double(), torch.from_numpy(y).double()
 
