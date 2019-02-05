ef3ed8fe707d8cb9f8a4c7c9b18b375bffd97554
QiYao <oqiyao@gmail.com>
2019-02-05 20:19:42 +0800

    boundary aware

---
+++ layers/module/fmf.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from layers.module import Ushape
+
+
+#注意这个fmf输出是输入通道数的二倍
+class FMF(nn.Module):
+    def __init__(self, in_channels, out_channels, depth=5):
+        super(FMF, self).__init__()
+        self.u = Ushape.UNet(out_channels, in_channels + 13, depth=depth, merge_mode='concat')
+        self.sig = nn.Sigmoid()
+
+        self.conv = nn.Conv2d(out_channels*2, out_channels, kernel_size=1, stride=1)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal(m.weight.data)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+        #第一个featurefusion其中heatmap缩放用的maxpooling，128到32
+    def forward(self, featuremap, heatmap):
+
+        heatmapall = F.interpolate(heatmap, featuremap.shape[2:4])
+
+        #print(heatmapall.shape)
+        #print(featuremap.shape)
+        x = torch.cat([heatmapall,featuremap],1)
+        #print(x.shape)
+        # x = np.c_[heatmapall, featuremap]
+        out = self.u(x)
+        out = self.sig(out)
+        out = out * featuremap
+        #print(out.shape)
+        #out = np.c_[out, featuremap]
+        out = torch.cat([out,featuremap],1)
+        out = self.conv(out)
+        return out
+
+
--- models/dense201.py
+++ models/dense201.py
@@ -32,3 +32,16 @@ class Dense201(densenet.DenseNet):
         out = super(Dense201, self).forward(x)
         out = self.sigmoid(out)
         return out
+
+if __name__ == '__main__':
+    import numpy as np
+    import torch
+    # heatmap =np.zeros((128, 128, 3), np.uint8)
+    heatmap = torch.FloatTensor(np.zeros((1, 13, 128, 128), np.uint8))
+    # print(heatmap)
+    # print(heatmap.shape)
+    featuremap = torch.FloatTensor(np.random.random((1, 3, 256, 256)))
+    #假设featuremap是64通道的32*32
+    net = Dense201(num_classes=212)
+
+    out = net(featuremap)
