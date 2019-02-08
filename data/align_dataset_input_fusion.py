d2c43880025bac80d452c0fe102b987b0199d414
QiYao <oqiyao@gmail.com>
2019-02-08 13:47:05 +0800

    精简

--- data/align_dataset_input_fusion.py
+++ data/align_dataset_input_fusion.py
@@ -23,6 +23,21 @@ class AlignFusionDataset(AlignDataset):
         self.algin_ldmk = [os.path.join(al_ldmk_dir, f + '.txt') for f in self.file_list]
         self.kernel_size = kernel_size
         self.sigma = sigma
+        self.idxs = [list(range(32)),
+                     [51, 52, 53, 60],
+                     list(range(55, 66)),
+                     list(range(33, 38)),
+                     [33, 41, 40, 39, 38, 37],
+                     list(range(42, 47)),
+                     [46, 47, 48, 49, 50, 42],
+                     list(range(66, 71)),
+                     [66, 73, 72, 71, 70],
+                     list(range(75, 80)),
+                     [75, 82, 81, 80, 79],
+                     [84, 85, 86, 87, 88, 89, 90],
+                     [90, 100, 99, 98, 97, 96, 84],
+                     [84, 103, 102, 101, 90],
+                     [90, 91, 92, 93, 94, 95, 84]]
 
     def __getitem__(self, item):
         image, landmarks = super(AlignFusionDataset, self).__getitem__(item)
@@ -36,95 +51,12 @@ class AlignFusionDataset(AlignDataset):
         kernel_size = self.kernel_size
         sigma = self.sigma
         heatmaps = []
-        # 脸部外轮廓
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = list(range(32))
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
 
-        # 鼻梁
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = [51, 52, 53, 60]
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
+        for idx in self.idxs:
+            img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
+            draw_curve(img, landmarks, idx)
+            heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
 
-        # 鼻子下轮廓
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = list(range(55, 66))
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 左眉上
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = list(range(33, 38))
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 左眉下
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = [33, 41, 40, 39, 38, 37]
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 右眉上
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = list(range(42, 47))
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 右眉下
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = [46, 47, 48, 49, 50, 42]
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 左眼上
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = list(range(66, 71))
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 左眼下
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = [66, 73, 72, 71, 70]
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 右眼上
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = list(range(75, 80))
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 右眼下
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = [75, 82, 81, 80, 79]
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 上嘴唇上
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = [84, 85, 86, 87, 88, 89, 90]
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 上嘴唇下
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = [90, 100, 99, 98, 97, 96, 84]
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 下嘴唇上
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = [84, 103, 102, 101, 90]
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
-
-        # 下嘴唇下
-        img = np.zeros((self.shape[0], self.shape[1]), np.uint8)
-        idx = [90, 91, 92, 93, 94, 95, 84]
-        draw_curve(img, landmarks, idx)
-        heatmaps.append(cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma))
         inputs = []
         for i in range(len(heatmaps)):
             heatmaps[i] = heatmaps[i].astype(np.float32) / np.max(heatmaps[i])
