74ee9208312798e700ae5d51a61935edcbe81340
QiYao <oqiyao@gmail.com>
2019-02-12 16:04:06 +0800

    id

--- layers/module/wing_loss2.py
+++ layers/module/wing_loss2.py
@@ -1,5 +1,7 @@
 from layers.module import wing_loss
 from torch import nn
+
+
 class WingLoss2(nn.Module):
     def __init__(self, w1, epsilon1, w2, epsilon2):
         super(WingLoss2, self).__init__()
@@ -7,6 +9,6 @@ class WingLoss2(nn.Module):
         self.wing2 = wing_loss.WingLoss(w2, epsilon2)
 
     def forward(self, predictions, targets):
-        loss1 = self.wing1(predictions[:34, :], targets[:34, :])
-        loss2 = self.wing2(predictions[34:, :], targets[34:, :])
+        loss1 = self.wing1(predictions[:33, :], targets[:33, :])
+        loss2 = self.wing2(predictions[33:, :], targets[33:, :])
         return loss1 + loss2
