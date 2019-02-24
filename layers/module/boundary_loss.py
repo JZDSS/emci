a3f1b82af1c1e970d3999bcb2c3fcff1bc42b5fe
QiYao <oqiyao@gmail.com>
2019-02-24 22:12:28 +0800

    bloss1

--- layers/module/boundary_loss.py
+++ layers/module/boundary_loss.py
@@ -1,32 +1,57 @@
 import torch
 from torch import nn
+import numpy as np
 import logging
+import sys
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S',
                     filemode='w')
 
+
 class BoundaryLoss(nn.Module):
 
-    def __init__(self, decay_threshold=10, threshold_decay_rate=0.9, mean_decay=0.99):
+    def __init__(self, decay_threshold=0.1, threshold_decay_rate=0.99, mean_decay=0.99):
         super(BoundaryLoss, self).__init__()
         self.threshold = 1
-        self.mean_loss = 0
+        self.mean_loss = 0.1
         self.threshold_decay_rate = threshold_decay_rate
         self.decay_threshold = decay_threshold
         self.mean_decay = mean_decay
         self.k = 1 - mean_decay
+        self.count = 0
+        self.zero = None
+
 
     def forward(self, prediction, target):
-        l2 = (prediction - target) ** 2
-        suppressed = torch.where(l2 <= self.threshold, torch.zeros_like(l2), l2).sum(dim=1).mean()
-        self.mean_loss *= self.mean_decay
-        self.mean_loss += self.k * suppressed
-        if self.mean_loss < self.decay_threshold:
-            self.threshold *= self.threshold_decay_rate
-            logging.info("threshold update: %f" % self.threshold)
+        if self.zero is None:
+            self.zero = torch.zeros_like(target)
+        l1 = torch.abs(prediction - target)
+
+        suppressed = torch.where(l1 < self.threshold, self.zero, l1).sum(dim=1).mean()
         return suppressed
 
+    def update(self, loss):
+        # if loss <=1e-3:
+        #     self.threshold /= 2
+        #     logging.info('threshold update: %e' % self.threshold)
+        #     self.mean_loss = loss
+        #     return
+        self.mean_loss *= self.mean_decay
+        self.mean_loss += self.k * loss
+        if loss < self.mean_loss:
+            self.count += 1
+        else:
+            self.count -= 1
+
+        if self.count == 3:
+            self.threshold *= self.threshold_decay_rate
+            logging.info("threshold update: %e" % self.threshold)
+            self.count = 0
+        elif self.count == -1:
+            self.threshold /= self.threshold_decay_rate
+            logging.info("threshold update: %e" % self.threshold)
+            self.count = 0
 
 class BoundaryLoss2(nn.Module):
     def __init__(self, decay_threshold1, decay_rate1, mean_decay1,
