fa54e2fea19f679920d32ba30e83ab8bb04a6ca2
QiYao <oqiyao@gmail.com>
2019-03-12 20:05:04 +0800

    加了各种aug的bboxdataset

--- data/bbox_dataset.py
+++ data/bbox_dataset.py
@@ -13,35 +13,89 @@ class BBoxDataset(FaceDataset):
                  bins=[1,2,3,4,5,6,7,8,9,10,11],
                  phase='train',
                  shape=(224, 224),
-                 max_jitter=5,
+                 max_jitter=30,
+                 max_rand=30,
                  img_format=None):
         super(BBoxDataset, self).__init__(img_dir, ldmk_dir, bin_dir, bins, phase, shape, img_format)
         # self.bboxes = [os.path.join(bbox_dir, f + '.rect') for f in self.file_list]
         self.max_jitter = max_jitter
+        self.max_rand = max_rand / 180 * np.pi
 
     def __getitem__(self, item):
         image, landmarks = super(BBoxDataset, self).__getitem__(item)
-        # bbox_path = self.bboxes[item]
-        # try:
-        #     bbox = utils.read_bbox(bbox_path)
-        # except:
-        bbox = [0, 0, image.shape[1] - 1, image.shape[0] - 1]
-        minx, miny, maxx, maxy = bbox
+
+        H, W, _ = image.shape
+        # landmarks /= [W, H]
+        resize = \
+            np.array([
+                [self.shape[0] / W, 0, 0],
+                [0, self.shape[1] / H, 0],
+                [0, 0, 1]],
+                dtype=np.float32)
         if self.phase == 'train':
-            left = min(minx, self.max_jitter)
-            right = min(image.shape[1] - maxx - 1, self.max_jitter)
-            up = min(miny, self.max_jitter)
-            down = min(image.shape[0] - maxy - 1, self.max_jitter)
-            dh = np.random.randint(-up, down+1, 1)
-            dv = np.random.randint(-left, right+1, 1)
-            bbox[0::2] += dv
-            bbox[1::2] += dh
-        minx, miny, maxx, maxy = bbox
-        landmarks = utils.norm_landmarks(landmarks, bbox)
-        image = image[miny:maxy + 1, minx:maxx + 1, :]
-        image = cv2.resize(image, self.shape)
+            theta = np.random.uniform(-self.max_rand, self.max_rand)
+            dx = np.random.uniform(-self.max_jitter, self.max_jitter)
+            dy = np.random.uniform(-self.max_jitter, self.max_jitter)
+            dice = np.random.uniform(0, 1)
+            if dice > 0.5:
+                flip = \
+                    np.array([
+                        [1, 0, self.shape[1]],
+                        [0, 1, 0],
+                        [0, 0, 1]],
+                        dtype=np.float32
+                    ) @ \
+                    np.array([
+                        [-1, 0, 0],
+                        [0, 1, 0],
+                        [0, 0, 1]],
+                        dtype=np.float32)
+            else:
+                flip = np.identity(3, np.float32)
+            center = \
+                np.array([
+                    [1, 0, -self.shape[1] / 2],
+                    [0, 1, -self.shape[1] / 2],
+                    [0, 0, 1]],
+                    dtype=np.float32
+                )
+
+            c = np.cos(theta)
+            s = np.sin(theta)
+            rotation = \
+                np.array([
+                    [c, s, 0],
+                    [-s, c, 0],
+                    [0, 0, 1]],
+                    dtype=np.float32
+                )
+
+            decenter = \
+                np.array([
+                    [1, 0, self.shape[1] / 2],
+                    [0, 1, self.shape[1] / 2],
+                    [0, 0, 1]],
+                    dtype=np.float32
+                )
+            rotation = decenter @ rotation @ center
+
+            translate = \
+                np.array([
+                    [1, 0, dx],
+                    [0, 1, dy],
+                    [0, 0, 1]],
+                    dtype=np.float32
+                )
+            T = translate @ rotation @ flip @ resize
+        else:
+            T = resize
+
+
+        image = cv2.warpAffine(image, T[:2, :], self.shape)
+        ones = np.ones(len(landmarks), dtype=np.float32)
+        x = np.c_[landmarks, ones]
+        landmarks = x @ np.transpose(T[:2, :])
         if self.phase == 'train':
-            image, landmarks = utils.random_flip(image, landmarks, 0.5)
             image = utils.random_gamma_trans(image, np.random.uniform(0.8, 1.2, 1))
             image = utils.random_color(image)
 
@@ -52,6 +106,13 @@ class BBoxDataset(FaceDataset):
 if __name__ == '__main__':
     a = BBoxDataset('/data/icme/crop/data/picture',
                     '/data/icme/crop/data/landmark',
-                    '/data/icme/bbox',
-                    '/data/icme/train')
-    a.__getitem__(1)
+                    '/data/icme/train',
+                    img_format='png')
+    import matplotlib.pyplot as plt
+    for i in range(100):
+        image, landmark = a.__getitem__(i)
+        image = np.transpose(image, (1, 2, 0))[:, :, ::-1].astype(np.uint8)
+        landmark = np.reshape(landmark, [-1, 2])
+        plt.imshow(image)
+        plt.scatter(landmark[:, 0], landmark[:, 1])
+        plt.show()
